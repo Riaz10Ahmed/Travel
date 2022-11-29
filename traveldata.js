012d7141624ad72970cbb94d0d6a0b7ef7b8aa28
Riaz Ahmed <riazahmedm@anyaconsultancy.com>
2022-11-29 10:48:49 +0530

    common json added & categories updated

---
+++ traveldata.js
@@ -0,0 +1,113 @@
+var da = `[
+    {
+        "destId":1001,
+        "destName": "Ooty",
+        "destTitle": "Mesmerizing views | Wildlife | Boating | Horticulture | Food",
+        "destDesc": "Also known as the Land of the Gods, Ooty appeals through its sheer natural beauty of looming volcanoes and lush terraced rice fields that exude peace and serenity. It is also famous for surfers' paradise! Bali enchants with its dramatic dances and colorful ceremonies, its arts, and crafts, to its luxurious beach resorts and exciting nightlife.",
+        "destImg": "./img/ooty-startexp.jpg",
+        "categoryTag": "family honeymoon roadtrip",
+        "redirectUrl": "./ooty.html",
+        "destSubTitle": "5 Nights 6 Days",
+        "packages":[
+            {
+                "packageName": "",
+                "packageTitle": "",
+                "packageImg": "",
+                "packagePriceStartingFrom": "",
+                "packageDesc": "",
+                "packageSubTitle": "",
+                "packageNoOfActivities": "",
+                "packageCategoryTag": "",
+                "packageIncluded": [
+                    {
+                        "includedImg": "",
+                        "includedTitle": "",
+                        "includedDesc": ""
+                    }
+                ],
+                "packageItinerary":[
+                    {
+                        "itnTitle": "",
+                        "itnDesc": "",
+                        "itnImg": "",
+                        "itnPlacesToVisit": [
+                            {
+                                "placeTitle": "",
+                                "placeDesc": "" 
+                            },
+                            {
+                                "placeTitle": "",
+                                "placeDesc": "" 
+                            }
+                        ],
+                        "itnThingsToDo": [
+                            {
+                                "ttdTitle": "",
+                                "ttdDesc": ""
+                            },
+                            {
+                                "ttdTitle": "",
+                                "ttdDesc": ""
+                            }
+                        ],
+                        "itnNotes": ""
+                    }
+                ],
+                "highlights":[
+                    {
+                        "highlightTitle": "",
+                        "highlightDesc": "",
+                        "highlightImg": ""
+                    },
+                    {
+                        "highlightTitle": "",
+                        "highlightDesc": "",
+                        "highlightImg": ""
+                    },
+                    {
+                        "highlightTitle": "",
+                        "highlightDesc": "",
+                        "highlightImg": ""
+                    }
+                ]
+            }
+        ]
+        
+    },
+    {
+        "destId":1002,
+        "destName": "Munnar",
+        "destTitle": "Above the Clouds | Falls | Adventure | Hanging Bridge",
+        "destDesc": "",
+        "destImg": "./img/munnar-startexp.jpg",
+        "categoryTag":"adventure honeymoon family roadtrip",
+        "redirectUrl": "./ooty.html"
+    },
+    {
+        "destId":1003,
+        "destName": "Wayanad",
+        "destTitle": "Stunning Views | Rejuvanating Falls | Zipline | Glass Bridge",
+        "destDesc": "",
+        "destImg": "./img/wayanad-startexp.jpg",
+        "categoryTag":"adventure family honeymoon roadtrip",
+        "redirectUrl": "./ooty.html"
+    },
+    {
+        "destId":1004,
+        "destName": "Kodaikanal",
+        "destTitle": "Trekking | Camping | Valleys | Grassy Hills | Evergreen Forest",
+        "destDesc": "",
+        "destImg": "./img/kodai-startexp.jpg",
+        "categoryTag":"family honeymoon roadtrip",
+        "redirectUrl": "./ooty.html"
+    },
+    {
+        "destId":1005,
+        "destName": "Manali",
+        "destTitle": "Snowy Mountains | Adrenaline | Paragliding | River Rafting",
+        "destDesc": "",
+        "destImg": "./img/manali-startexp.jpg",
+        "categoryTag":"adventure family honeymoon",
+        "redirectUrl": "./ooty.html"
+    }
+]`;
